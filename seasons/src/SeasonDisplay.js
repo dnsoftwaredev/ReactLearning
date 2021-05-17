414ff71b08a2e499bcdc82b12ea50ba027102273
Danh <dnsoftwarestore@gmail.com>
2021-05-17 14:02:40 -0500

    section6

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import './SeasonDisplay.css';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>;
+const seasonConfig = {
+    summer : {
+        text: 'Let\'s hit the beach!',
+        iconName: 'sun',
+    },
+    winter: {
+        text: 'Burr it is cold',
+        iconName: 'snowflake',
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    // season can return either 'summer' or 'winter' => pass to the line below
+    // to select summer or winter
+    const {text, iconName} = seasonConfig[season];
+    // syntaxtic sugar => deconstructuring
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon massive icon-left`} />
+            <h1>{text}</h1>
+            <i className={`${iconName} icon massive icon-right`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' };
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setState!!
-                this.setState({ lat: position.coords.latitude });
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
 
-                // WE DID NOT
-                // this.state.lat = position.coords.latitude
-                // this is fucking horrible do not do
-                // the only time to use direct assignment is in the beginning => this.state = { lat : null }
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            // when this.setState is call the component App got rerendered => the children
+            // components like SeasonDisplay also got rerendered with new data
         );
     }
 
-
-    // React says we have to define render!!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
+            // everytime componentDidMount is called => App component AND SeasonDisplay component got rerender
         }
 
         return (
-            <div>Loading!</div>
+            <Spinner message="Please accept location request"/>
         );
+    }
 
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );      
     }
 }
 
