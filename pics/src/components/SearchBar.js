7aab006c9825ad0050d7f9fb0e9334acf458d779
Danh <dnsoftwarestore@gmail.com>
2021-05-19 10:39:45 -0500

    section7

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        this.props.onSubmit(this.state.term);
+    }
+
+    render() {
+        // even this.onInputChange is a function
+        // you don't want to put () behind it because if you do that
+        // onInputChange is called everytime render got called
+        // you only want to call onInputChange when, well, input is changed
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
