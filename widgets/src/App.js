d15574cc6d2e46169228eba264fc0a310d0c93db
Danh <dnsoftwarestore@gmail.com>
2021-05-26 13:04:42 -0500

    section 12

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
     {
@@ -20,27 +21,24 @@ const items = [
 
 const options = [
     {
-        label: 'The Color Red',
+        label: 'Red',
         value: 'red'
     },
     {
-        label: 'The Color Green',
+        label: 'Green',
         value: 'green'
     },
     {
-        label: 'A Shade of Blue',
+        label: 'Blue',
         value: 'blue'
     }
 ];
 
 export default () => {
-    const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setShowDropdown] = useState(true);
 
     return (
         <div>
-            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-            {showDropdown ? <Dropdown selected={selected} onSelectedChange={setSelected} options={options} /> : null}
+            <Translate />
         </div>
     );
 };
